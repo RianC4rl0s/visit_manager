e02627e3d3a310b8df64cdcca3a5ba82c899be4f
RianC4rl0s <riancarlos.sl@gmail.com>
2023-10-03 19:21:00 -0300

    refac:default route

--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,5 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  #root "main#index"
+  root "visits#index"
 end
