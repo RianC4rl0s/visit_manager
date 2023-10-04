43b2cdc51d98b31b43c812c81d5270e1fa38b3e2
RianC4rl0s <riancarlos.sl@gmail.com>
2023-10-04 13:24:37 -0300

    webcam bugada

--- app/assets/javascripts/webcam.js
+++ app/assets/javascripts/webcam.js
@@ -17,35 +17,35 @@
 //     }
 //   });
 
-document.addEventListener("visibilitychange", function () {
-    console.log("Visibility state: " + document.visibilityState);
-    if (document.visibilityState === "hidden") {
-        // Página ficou invisível (usuário saiu da tela)
-        stopCameraStream();
-    } else if (document.visibilityState === "visible") {
-        // Página ficou visível novamente (usuário voltou à tela)
-        startCameraStream();
-    }
-});
+// document.addEventListener("visibilitychange", function () {
+//     console.log("Visibility state: " + document.visibilityState);
+//     if (document.visibilityState === "hidden") {
+//         // Página ficou invisível (usuário saiu da tela)
+//         stopCameraStream();
+//     } else if (document.visibilityState === "visible") {
+//         // Página ficou visível novamente (usuário voltou à tela)
+//         startCameraStream();
+//     }
+// });
 
-function startCameraStream() {
-    navigator.mediaDevices
-        .getUserMedia({ video: true })
-        .then(function (stream) {
-            // Associar o stream da webcam ao elemento <video>
-            videoElement.srcObject = stream;
-        })
-        .catch(function (error) {
-            console.error("Erro ao acessar a webcam: ", error);
-        });
-}
+// function startCameraStream() {
+//     navigator.mediaDevices
+//         .getUserMedia({ video: true })
+//         .then(function (stream) {
+//             // Associar o stream da webcam ao elemento <video>
+//             videoElement.srcObject = stream;
+//         })
+//         .catch(function (error) {
+//             console.error("Erro ao acessar a webcam: ", error);
+//         });
+// }
 
-function stopCameraStream() {
-    if (videoElement.srcObject) {
-        var tracks = videoElement.srcObject.getTracks();
-        tracks.forEach(function (track) {
-            track.stop();
-        });
-        videoElement.srcObject = null;
-    }
-}
+// function stopCameraStream() {
+//     if (videoElement.srcObject) {
+//         var tracks = videoElement.srcObject.getTracks();
+//         tracks.forEach(function (track) {
+//             track.stop();
+//         });
+//         videoElement.srcObject = null;
+//     }
+// }
